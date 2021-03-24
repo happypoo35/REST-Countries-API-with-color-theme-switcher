6e0bb16771a38a7125e348fd23959ec992a986f7
Alex <happypoo35@gmail.com>
2021-03-24 18:26:24 +0300

    error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,16 @@
+import { BsArrowLeft } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
-    <div>
-      <h2>Hello from Error page</h2>
+    <div className="empty-result">
+      <h2>This page doesn't exist</h2>
+      <Link to="/" aria-label="previous page">
+        <button className="btn btn-back">
+          <BsArrowLeft />
+          Back
+        </button>
+      </Link>
     </div>
   );
 };
